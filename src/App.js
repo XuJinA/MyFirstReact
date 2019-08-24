d93465ac1135ff63c56c7e2cc9048370684a1a8c
XuJinA <752595547@qq.com>
2019-08-24 20:54:47 +0800

    one

--- src/App.js
+++ src/App.js
@@ -23,4 +23,58 @@ function App() {
   );
 }
 
-export default App;
+class Clock1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Clock1 Hello, world!</h1>
+        <h2>现在是 {this.props.date1.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>Clock Hello, world!</h1>
+      <h2>现在是 {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function Tick1() {
+  return(
+    <div>
+    <Clock date={new Date()} />
+    <Clock1 date1={new Date()} />
+    </div>
+  );
+}
+
+function App2() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsaa2222</code> and save to reload.
+          
+        </p>
+        <Tick1></Tick1>
+        
+        <Clock1 date1={new Date()} />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Name(props) {
+    return <h1>网站名称：{props.name}</h1>;
+}
+function Url(props) {
+    return <h1>网站地址：{props.url}</h1>;
+}
+function Nickname(props) {
+    return <h1>网站小名：{props.nickname}</h1>;
+}
+function App3() {
+    return (
+    <div>
+        <Name name="菜鸟教程" />
+        <Url url="http://www.runoob.com" />
+        <Nickname nickname="Runoob" />
+    </div>
+    );
+}
+
+function tick(){
+    ReactDOM.render(
+        <div>            
+        <App />
+            <App3 />
+        </div>, 
+        document.getElementById('root'));
+}
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
